28e0599dfa8d6161fa017e99ea751920cbb416c1
Jonas <72883058+jokrass99@users.noreply.github.com>
2021-03-13 01:57:32 +0000

    Add files via upload

---
+++ MacLookup.py
@@ -0,0 +1,18 @@
+import os
+
+while True:
+
+    print('\n >>> Please enter the mac address: ')
+    macInput = raw_input()
+
+    print('\n >>> Mac Address Vendor: ')
+
+    os.system('curl http://api.macvendors.com/' + macInput)
+
+    print('\n')
+    inp = raw_input('\n >>> Would you like to check another mac address? (y/n): ')
+    if(inp == "y" or inp == "Y"):
+      continue
+    else:
+      print '\n Ciao!\n'
+      break
