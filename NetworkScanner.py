2f754855e8fbf9d6bebb2be42c53bef4157d5f3a
Jonas <72883058+jokrass99@users.noreply.github.com>
2021-04-14 17:23:28 +0000

    Update NetworkScanner.py

--- NetworkScanner.py
+++ NetworkScanner.py
@@ -22,8 +22,13 @@ while True:
       ip_add_range_entered = input('\nPlease enter the ip address and range that you want to send the ARP request to (ex 192.168.1.0/24): ')
     
       if ip_add_range_pattern.search(ip_add_range_entered):
-            print(f"{ip_add_range_entered} is a valid ip address range")
+            print('\n')
+            print(f'{ip_add_range_entered} is a valid ip address range')
+            print('\n')
             break
 
 arp_result = scapy.arping(ip_add_range_entered)
+
+print('\n')
+
 
