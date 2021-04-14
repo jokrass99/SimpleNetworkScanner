2c647b1a359faadcec499393ece3ac6cdd948cad
Jonas <72883058+jokrass99@users.noreply.github.com>
2021-04-14 16:45:34 +0000

    Update NetworkScanner.py

--- NetworkScanner.py
+++ NetworkScanner.py
@@ -1,4 +1,3 @@
-# Import scapy
 import scapy.all as scapy
 
 import re
@@ -15,18 +14,20 @@ print(r"""
 ╱╱╱╱╱╱╱╱╰╯    >>> SɪᴍᴘʟᴇNᴇᴛᴡᴏʀᴋSᴄᴀɴɴᴇʀ     """)
 
 
-print("\n      * GitHub: https://github.com/jokrass99 *")
+print('\n      * GitHub: https://github.com/jokrass99 *')
 
 
 
-ip_add_range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
+ip_add_range_pattern = re.compile('^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$')
 
-# Get the address range to ARP
 while True:
-    ip_add_range_entered = input("\nPlease enter the ip address and range that you want to send the ARP request to (ex 192.168.1.0/24): ")
-    if ip_add_range_pattern.search(ip_add_range_entered):
-        print(f"{ip_add_range_entered} is a valid ip address range")
-        break
-
+    
+      ip_add_range_entered = input('\nPlease enter the ip address and range that you want to send the ARP request to (ex 192.168.1.0/24): ')
+    
+      if ip_add_range_pattern.search(ip_add_range_entered):
+        
+      print(f'{ip_add_range_entered} is a valid ip address range')
+        
+         break
 
 arp_result = scapy.arping(ip_add_range_entered)
